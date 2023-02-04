682bcadaa8cfb04cad51d1564de0ec439b18b251
Kayo Cordeiro <kayo@outlook.in>
2023-02-04 14:10:03 -0300

    fix

--- init-container.sh
+++ init-container.sh
@@ -38,10 +38,11 @@ if [[ "$WEBSITE_HOSTNAME" == *"azurewebsites.net"* ]]; then
         echo "Laravel app is not installed, laravel workers will not be configured"
     fi
 
-    if [ -z ${GH_TOKEN+x}] then
+    if [ -z ${GH_TOKEN+x}]; then
     echo "Update Git credentials"
     cd /home/site & gh auth setup-git
     fi
+    
 else
     echo "Running on local"
     mkdir -p /home/site/wwwroot
