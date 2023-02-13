f3628de549d5f01632e5d2248fe58536ff9170b8
Kayo Cordeiro <kayo@outlook.in>
2023-02-13 14:34:56 -0300

    modified:   .docker/run.d/update-repo.sh

--- .docker/run.d/update-repo.sh
+++ .docker/run.d/update-repo.sh
@@ -8,18 +8,29 @@ fi
 cd /home/site/wwwroot
 
 echo "Fetching updates from remote repository..."
-git fetch $REPO_NAME
+git fetch origin
 
 echo "Checking for updates..."
-if [ $(git rev-parse HEAD) != $(git rev-parse $REPO_NAME/$REPO_BRANCH) ]; then
+if [ $(git rev-parse HEAD) != $(git rev-parse origin/$REPO_BRANCH) ]; then
   echo "New updates found. Updating..."
-  git merge $REPO_NAME/$REPO_BRANCH
+  git merge origin/$REPO_BRANCH
   echo "Repository updated successfully."
 
   echo "Updating Laravel App..."
+  echo "Updating composer packages..."
   composer install --no-dev --prefer-dist --optimize-autoloader
+  echo "Restarting application..."
+  supervisorctl restart all
+  echo "Application restarted successfully."
+  echo "Updating database..."
   php artisan migrate --force
+  echo "Database updated successfully."
+  echo "Seeding database..."
   php artisan db:seed --force
+  echo "Database seeded successfully."
+
+  echo "Laravel App updated successfully."
+  
 else
   echo "No updates found."
 fi
