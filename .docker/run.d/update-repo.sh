8b2758b8083885dd1fe0fa15e4b29bebf2228c34
Kayo Cordeiro <kayo@outlook.in>
2023-02-13 14:34:39 -0300

    modified:   .docker/run.d/update-repo.sh

--- .docker/run.d/update-repo.sh
+++ .docker/run.d/update-repo.sh
@@ -15,19 +15,22 @@ if [ $(git rev-parse HEAD) != $(git rev-parse origin/$REPO_BRANCH) ]; then
   echo "New updates found. Updating..."
   git merge origin/$REPO_BRANCH
   echo "Repository updated successfully."
+
+  echo "Updating Laravel App..."
+  echo "Updating composer packages..."
+  composer install --no-dev --prefer-dist --optimize-autoloader
   echo "Restarting application..."
   supervisorctl restart all
   echo "Application restarted successfully."
-
-  echo "Updating Laravel App..."
-  composer install --no-dev --prefer-dist --optimize-autoloader
+  echo "Updating database..."
   php artisan migrate --force
+  echo "Database updated successfully."
+  echo "Seeding database..."
   php artisan db:seed --force
+  echo "Database seeded successfully."
 
-  echo "Updating NPM Packages..."
-  npm install -G npm
-  npm install
-  npm run production
+  echo "Laravel App updated successfully."
+  
 else
   echo "No updates found."
 fi
