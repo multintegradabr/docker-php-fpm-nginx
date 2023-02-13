ed6edc9423fb890d91719e4c8df10e782ae830ab
Kayo Cordeiro <kayo@outlook.in>
2023-02-13 15:22:05 -0300

    modified:   .docker/run.d/update-repo.sh

--- .docker/run.d/update-repo.sh
+++ .docker/run.d/update-repo.sh
@@ -7,30 +7,39 @@ fi
 
 cd /home/site/wwwroot
 
-echo "Fetching updates from remote repository..."
-git fetch origin
+if [ -d "/home/site/wwwroot/.git" ]; then
+  echo "Fetching updates from remote repository..."
+  git fetch origin
 
-echo "Checking for updates..."
-if [ $(git rev-parse HEAD) != $(git rev-parse origin/$REPO_BRANCH) ]; then
-  echo "New updates found. Updating..."
-  git merge origin/$REPO_BRANCH
-  echo "Repository updated successfully."
+  echo "Checking for updates..."
+    if [ $(git rev-parse HEAD) != $(git rev-parse origin/$REPO_BRANCH) ]; then
+    echo "New updates found. Updating..."
+    git merge origin/$REPO_BRANCH
+    echo "Repository updated successfully."
 
-  echo "Updating Laravel App..."
-  echo "Updating composer packages..."
-  composer install --no-dev --prefer-dist --optimize-autoloader
-  echo "Restarting application..."
-  supervisorctl restart all
-  echo "Application restarted successfully."
-  echo "Updating database..."
-  php artisan migrate --force
-  echo "Database updated successfully."
-  echo "Seeding database..."
-  php artisan db:seed --force
-  echo "Database seeded successfully."
+    echo "Updating Laravel App..."
+    
+    echo "Updating composer packages..."
+    composer install --no-dev --prefer-dist --optimize-autoloader
 
-  echo "Laravel App updated successfully."
-  
+    echo "Updating database..."
+    #php artisan migrate --force
+    #echo "Database updated successfully."
+    
+    #echo "Seeding database..."
+    #php artisan db:seed --force
+    #echo "Database seeded successfully."
+
+    echo "Laravel App updated successfully."
+
+    echo "Restarting application..."
+    supervisorctl restart all
+    echo "Application restarted successfully."
+      
+    else
+      echo "No updates found."
+    fi  
 else
-  echo "No updates found."
+  echo "Git folder does not exist, exiting"
+  exit 0
 fi
