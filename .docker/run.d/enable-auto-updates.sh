5bb6ab6a109e4645069f8d51d7c89a14d875245d
Kayo Cordeiro <kayo@outlook.in>
2023-02-13 12:02:09 -0300

    update repo after commit

--- .docker/run.d/enable-auto-updates.sh
+++ .docker/run.d/enable-auto-updates.sh
@@ -3,7 +3,9 @@
 echo "Enabling auto updates after merge..."
 
 mv /home/site/run.d/post-merge /home/site/wwwroot/.git/hooks/post-merge
+mv /home/site/run.d/post-commit /home/site/wwwroot/.git/hooks/post-commit
 
 chmod +x /home/site/wwwroot/.git/hooks/post-merge
+chmod +x /home/site/wwwroot/.git/hooks/post-commit
 
-echo "Successfully enabled auto updates after merge!"
+echo "Successfully enabled auto updates after merge and commits."
