efe05039545cf07f97ab36f7626e7cf6cc651a1a
Kayo Cordeiro <kayo@outlook.in>
2023-03-20 16:11:47 -0300

    modified:   .docker/run.d/update-repo.sh

--- .docker/run.d/update-repo.sh
+++ .docker/run.d/update-repo.sh
@@ -14,7 +14,7 @@ if [ -d "/home/site/wwwroot/.git" ]; then
   echo "Checking for updates..."
     if [ $(git rev-parse HEAD) != $(git rev-parse origin/$REPO_BRANCH) ]; then
     echo "New updates found. Updating..."
-    git reset --hard HEAD
+    git restore .
     git pull origin $REPO_BRANCH
     echo "Repository updated successfully."
 
