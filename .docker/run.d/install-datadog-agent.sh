ce45ddeb8345433cb9cd251a544740c04ee17308
Kayo Cordeiro <kayo@outlook.in>
2023-10-04 16:31:14 -0300

    fix datadog

--- .docker/run.d/install-datadog-agent.sh
+++ .docker/run.d/install-datadog-agent.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Installing Datadog Agent"
-export DD_HOSTNAME=$WEBSITE_SITE_NAME
+echo "Download Datadog Agent"
+export DD_HOSTNAME=$HOSTNAME
 DD_API_KEY=4190390b821cd76e0f809161f3386d3a DD_SITE="datadoghq.com" DD_INSTALL_ONLY=true bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
 echo "Configuring Datadog Agent"
-mv /etc/datadog-agent/security-agent.yaml.example security-agent.yaml 
+cp /etc/datadog-agent/security-agent.yaml.example /etc/datadog-agent/security-agent.yaml
+
+echo "Fixing permissions"
+sudo sh -c "chown dd-agent:dd-agent /etc/datadog-agent/datadog.yaml && chmod 640 /etc/datadog-agent/datadog.yaml"
 
 echo "Starting Datadog Agent"
 service datadog-agent start
